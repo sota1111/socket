5f663e0e3897f9bbef01f7f73cb5666ef7bb3051
sota1111 <sota.moro@gmail.com>
2023-10-27 15:19:56 +0900

    socketをリファクタリング

--- client_dart/lib/socket.dart
+++ client_dart/lib/socket.dart
@@ -1,40 +1,37 @@
 import 'dart:io';
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'socket_log.dart';
 
 const env = String.fromEnvironment('ENV', defaultValue: 'dev');
+const portController = '55001';
 
 final socketComProvider = StateNotifierProvider<SocketCom, void>((ref) {
   return SocketCom.instance;
 });
 class SocketCom extends StateNotifier {
+  Socket? socket;
   LogManager? logManager;
   SocketCom._(this.logManager) : super(null);
   static final SocketCom _instance = SocketCom._(null);
-
   static SocketCom get instance => _instance;
-  final _portController = '55001';
-  Socket? socket;
+
   bool compInitComm = false;
-
-  void _onDataReceived() {
-  }
-
   bool dialogIsShown = false;
+
   Future<void> socketConnect() async {
     const addressController = (env == 'dev') ? '127.0.0.1':'192.168.42.100';
     final serverAddress = InternetAddress(addressController);
-    final serverPort = int.parse(_portController);
+    final serverPort = int.parse(portController);
 
     try {
-      socket = await Socket.connect(serverAddress, serverPort,
-          timeout: const Duration(seconds: 5));
+      socket = await Socket.connect(serverAddress, serverPort, timeout: const Duration(seconds: 5));
       debugPrint('socket connected');
       logManager?.logToFile('socket connected');
-      socket!.listen(
-            (data) => _onDataReceived(),
+      socket!.listen((data) => _onDataReceived(),
         onDone: () {
           socket = null;
         },
@@ -42,43 +39,7 @@ class SocketCom extends StateNotifier {
     } catch (e) {
       debugPrint('Connection Error: $e');
       logManager?.logToFile('Connection Error: $e');
-      //await _displayInstructions(context);
     }
-  }
-
-  Future<void>  _displayInstructions(BuildContext context) async {
-    dialogIsShown = true;
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return WillPopScope(
-          onWillPop: () async => false,
-          child: AlertDialog(
-            title: const Text('USBテザリングを\n有効にして下さい。'),
-            content: const Text(
-                '「アクセスポイントとテザリング」\n▶︎「USBテザリング」\n▶︎"ON"\n\n解決しない場合は、\nUSB接続を確認して下さい。'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  //await _openNetworkSettings();
-                  dialogIsShown = false;
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-
-  Future<void> _openNetworkSettings() async {
-    //const AndroidIntent intent = AndroidIntent(
-      //action: 'android.settings.WIRELESS_SETTINGS',
-    //);
-    //await intent.launch();
   }
 
   Future<void> socketSend(String message) async {
@@ -91,6 +52,9 @@ class SocketCom extends StateNotifier {
     logManager?.logToFile('Send Message: $message');
     debugPrint('Send Message: $message');
     socket.write(message);
+  }
+
+  void _onDataReceived() {
   }
 
   Future<void> socketDisconnect() async {
@@ -131,30 +95,26 @@ class SocketWidgetState extends ConsumerState<SocketWidget> {
       await socketCom.socketConnect();
       Future.delayed(const Duration(seconds: 5), () async {
         socketCom.compInitComm = true;
-        await socketCom.socketSend("Authentication,1");
-        restartTimer();
+        startTimer();
       });
     });
   }
-
 
   Future<void> timerAction() async {
     final socketCom = this.socketCom;
     if( (socketCom.socket != null) && (socketCom.compInitComm == true)) {
       socketCom.socketSend('send_message');
     } else {
-
       stopTimer();
       await socketCom.socketConnect();
-      await Future.delayed(const Duration(seconds: 2));
+      await Future.delayed(const Duration(seconds: 5));
       debugPrint('retry connection');
-      socketCom.socketSend("Authentication,1");
-      restartTimer();
+      startTimer();
     }
   }
 
   // タイマを再開するメソッド
-  void restartTimer() {
+  void startTimer() {
     _timer ??= Timer.periodic(
         const Duration(milliseconds: 1000), (timer) => timerAction());
   }
